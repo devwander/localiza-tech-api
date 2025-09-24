67bf440fa1f90483704eadc0ac0f77bdeb83f43d
devwander <josewanderson173@gmail.com>
2025-09-24 11:47:13 -0300

    feat: CORS config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
